158cddcb326cde17b1cc28bafd6d9da0129f2d9c
Mahmoud-Essam-Noureldin <mahmoudnoir24@gmail.com>
2024-04-20 21:58:26 +0200

    added comment

--- GPIO/gpio.h
+++ GPIO/gpio.h
@@ -22,10 +22,10 @@
 		#define BLUE      0x04
 		#define GREEN     0x08
 		#define PF123_mask             0x0E
-		#define PF04_mask               0x11
+		#define PF04_mask              0x11
 		#define PF_mask                0x20
 		#define PF_SW1_mask            0x10
-		#define PF_SW2_mask 
+		#define PF_SW2_mask            0x01
     
     // Like Lab 3
     // Initialize port F so that built-in switched are input, and built-in LEDs are output.

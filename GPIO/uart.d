8dd8617430730d828b5e7d528df1547b129a0563
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-04-20 18:38:16 +0200

    Deleted unnecessary files.

--- GPIO/uart.d
+++
@@ -1,4 +0,0 @@
-.\uart.o: uart.c
-.\uart.o: uart.h
-.\uart.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\uart.o: tm4c123gh6pm.h
--- GPIO/uart_test.d
+++
@@ -1,3 +0,0 @@
-.\uart_test.o: uart_test.c
-.\uart_test.o: uart.h
-.\uart_test.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h

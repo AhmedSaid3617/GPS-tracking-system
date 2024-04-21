bc4c1d4657a109f844705d00b0da312dbe8efd5a
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-04-21 23:08:59 +0200

    Modified UART1 to work with default pins.

--- UART/uart.c
+++ UART/uart.c
@@ -1,7 +1,7 @@
 #include "uart.h"
 #include "tm4c123gh6pm.h"
 
-#define SYSTEM_CLCK 16000000
+#define SYSTEM_CLCK 80000000
 
 void UART0_Init(int Baud_rate)
 {
@@ -34,23 +34,21 @@ void UART0_Init(int Baud_rate)
 void UART1_Init(int Baud_rate)
 {
   // Enable the clock for UART1
-  SYSCTL_RCGCUART_R |= 0x02; // Set bit 1 for UART1
+  SYSCTL_RCGCUART_R |= (1<<1); // Set bit 1 for UART1
 
-  // Enable clock for GPIO Port C
-  SYSCTL_RCGCGPIO_R |= 0x04; // Set bit 3 for GPIO Port A
+  // Enable clock for GPIO Port B
+  SYSCTL_RCGCGPIO_R |= (1<<1); // Set bit 2 for GPIO Port B
 
   // Wait for the clocks to be ready
-  while ((SYSCTL_PRGPIO_R & (2 << 1)) == 0)
-  {
-  } // Check bit 0 for Port C
+  while ((SYSCTL_PRGPIO_R & (1 << 1)) == 0) {} // Check bit 0 for Port C
   // while ((SYSCTL_PRUART_R & (1<<1)) == 0) {} // Check bit 0 for UART1
 
-  // Configure PC4 (RX) and PC5 (TX) as UART pins
-  GPIO_PORTC_AFSEL_R |= (1 << 4) | (1 << 5);             // Set RX and TX to alternate function
-  GPIO_PORTC_PCTL_R &= (GPIO_PORTA_PCTL_R & 0xFF00FFFF); // Clear PCTL bits for RX and TX
-  GPIO_PORTC_PCTL_R |= 0x000220000;                      // Set PCTL bits for RX and TX
-  GPIO_PORTC_DEN_R |= (1 << 0) | (1 << 1);               // Enable digital I/O for RX and TX pins
-  GPIO_PORTC_AMSEL_R &= ~(0x30);                         // Clear analog mode for RX and TX pins
+  // Configure PB0 (RX) and PB1 (TX) as UART pins
+  GPIO_PORTB_AFSEL_R |= (1 << 0) | (1 << 1);             // Set RX and TX to alternate function
+  GPIO_PORTB_PCTL_R &= (GPIO_PORTA_PCTL_R & 0xFFFFFF00); // Clear PCTL bits for RX and TX
+  GPIO_PORTB_PCTL_R |= 0x000000011;                      // Set PCTL bits for RX and TX
+  GPIO_PORTB_DEN_R |= (1 << 0) | (1 << 1);               // Enable digital I/O for RX and TX pins
+  GPIO_PORTB_AMSEL_R &= ~((1 << 0) | (1 << 1));                         // Clear analog mode for RX and TX pins
 
   // Set baud rate
   UART1_IBRD_R = (int)(SYSTEM_CLCK / (Baud_rate * 16));
--- UART/uart_test.c
+++ UART/uart_test.c
@@ -6,13 +6,13 @@ int main()
     uint8_t received;
     uint8_t array[100];
     uint8_t* array_pointer = &array[0];
-    UART0_Init(9600);
+    UART1_Init(9600);
     while (1)
     {
         // Try to receive a byte and store it in the array.
-        if(UART_ReceiveByte(UART0, array_pointer)){
+        if(UART_ReceiveByte(UART1, array_pointer)){
             // If a byte is received, send it and increment the pointer.
-            UART_SendByte(UART0, *array_pointer);
+            UART_SendByte(UART1, *array_pointer);
             array_pointer++;
         }
 

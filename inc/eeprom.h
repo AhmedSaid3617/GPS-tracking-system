bc2d648ce48f2b3fa17a78051465009fa73c510c
Shams M. Monem <shamsmmonem@gmail.com>
2024-04-21 16:03:54 +0200

    EEPROM initialization and drivers

---
+++ inc/eeprom.h
@@ -0,0 +1,72 @@
+#ifndef GPS_TRACKING_SYSTEM_EEPROM_H
+#define GPS_TRACKING_SYSTEM_EEPROM_H
+
+// TODO: Insert delay (6 cycles plus function call overhead)
+#define DELAY_6_CYCLES() {}
+
+// TODO: Insert delay (4 cycles plus function call overhead)
+#define DELAY_4_CYCLES() {}
+
+#define EEPROM *(volatile unsigned long*) 0x400AF000
+
+#define RCGCEEPROM ((volatile RCGCEEPROM_TypeDef *) 0x400FE658)
+#define EEDONE ((volatile EEDONE_TypeDef *) 0x400AF018)
+#define EESUPP ((volatile EESUPP_TypeDef *) 0x400AF01C)
+#define SREEPROM ((volatile SREEPROM_TypeDef *) 0x400FE558)
+
+#define EERDWR *((volatile unsigned short *) 0x400AF010)
+#define EEBLOCK ((volatile EEBLOCK_TypeDef *) 0x400AF004)
+#define EEOFFSET ((volatile EEOFFSET_TypeDef *) 0x400AF008)
+
+typedef enum EEPROM_Init_Status {
+    EEPROM_Init_Successfull,
+    EEPROM_Init_Failed,
+} EEPROM_Init_Status;
+
+typedef union RCGCEEPROM_TypeDef {
+    unsigned int R0: 1;
+    unsigned long value;
+} RCGCEEPROM_TypeDef;
+
+typedef union EEBLOCK_TypeDef {
+    unsigned int BLOCK: 16;
+    unsigned long value;
+} EEBLOCK_TypeDef;
+
+typedef union EEOFFSET_TypeDef {
+    unsigned int OFFSET: 4;
+    unsigned long value;
+} EEOFFSET_TypeDef;
+
+typedef union SREEPROM_TypeDef {
+    unsigned int R0: 1;
+    unsigned long value;
+} SREEPROM_TypeDef;
+
+
+typedef union EEDONE_TypeDef {
+    struct {
+        unsigned int WORKING: 1;
+        unsigned int _reserved: 1;
+        unsigned int WKERASE: 1;
+        unsigned int WKCOPY: 1;
+        unsigned int NOPERM: 1;
+        unsigned int WRBUSY: 1;
+    };
+    unsigned long value;
+} EEDONE_TypeDef;
+
+typedef union EESUPP_TypeDef {
+    struct {
+        unsigned int START: 1;
+        unsigned int EREQ: 1;
+        unsigned int ERETRY: 1;
+        unsigned int PERTRY: 1;
+    };
+    unsigned long value;
+} EESUPP_TypeDef;
+
+EEPROM_Init_Status EEPROM_Init();
+
+
+#endif //GPS_TRACKING_SYSTEM_EEPROM_H
---
+++ src/eeprom.c
@@ -0,0 +1,64 @@
+#include "eeprom.h"
+
+/*
+ * EEPROM Initialization (8.2.4.2)
+ * */
+EEPROM_Init_Status EEPROM_Init() {
+    RCGCEEPROM->R0 = 1;
+
+    DELAY_6_CYCLES();
+
+    while (EEDONE->WORKING == 1);
+
+    SREEPROM->R0 = 0;
+
+    DELAY_6_CYCLES();
+
+    while (EEDONE->WORKING == 1);
+
+    if (EESUPP->ERETRY || EESUPP->PERTRY) {
+        return EEPROM_Init_Failed;
+    }
+
+    return EEPROM_Init_Successfull;
+}
+
+/*
+ * Prepare the EEPROM to write or read data at [block] and [offset]
+ * */
+void EEPROM_Set_Block_And_Offset(unsigned int block, unsigned int offset) {
+    if (block > (1 << 16)) {
+        return;
+    }
+
+    if (offset > (1 << 4)) {
+        return;
+    }
+
+    EEBLOCK->BLOCK = block;
+    EEOFFSET->OFFSET = offset;
+
+    DELAY_4_CYCLES();
+}
+
+/*
+ * Write data at pseudo-address [address] in EEPROM
+ * */
+unsigned long EEPROM_Read(int address, unsigned long value) {
+    // TODO: convert address into block and offset
+    EEPROM_Set_Block_And_Offset(0, address);
+
+
+    return EERDWR;
+}
+
+/*
+ * Read data at pseudo-address [address] from EEPROM
+ * */
+void EEPROM_Write(int address, unsigned long value) {
+    // TODO: convert address into block and offset
+    EEPROM_Set_Block_And_Offset(0, address);
+
+
+    EERDWR = value;
+}

cdab1d86b46786e622940dddf57024d8adc812f2
Shams M. Monem <shamsmmonem@gmail.com>
2024-04-23 12:56:33 +0200

    GPS fix variable error

--- inc/gps.h
+++ inc/gps.h
@@ -1,6 +1,9 @@
 #ifndef GPS_TRACKING_SYSTEM_GPS_H
 #define GPS_TRACKING_SYSTEM_GPS_H
 
+#define GPS_UART_Fill_Buffer(buffer)
+#define GPS_EEPROM_Save(buffer)
+
 void GPS_Start();
 
 #endif //GPS_TRACKING_SYSTEM_GPS_H
--- src/gps.c
+++ src/gps.c
@@ -1,45 +1,57 @@
+#include "gps.h"
 #include <stddef.h>
 #include <string.h>
 #include <stdio.h>
 
 #define COORDINATES_SIZE 100
+#define BUFFER_SIZE 250
 
+/*
+ * Polls GPS for latitude, longitude, satellites used and altitude
+ * */
 void GPS_Start() {
-    const char *rmc = "$GPRMC"; // To get speed and so
-    const char *gga = "$GPGGA"; // To get satellite count
-    const char *rmc_ptr = NULL;
-    const char *gga_ptr = NULL;
-    float coordinates[COORDINATES_SIZE][2];
-    float (*coordinates_ptr)[2] = coordinates;
+    char buffer[BUFFER_SIZE];
 
-    rmc_ptr = strstr(data, rmc);
-    gga_ptr = strstr(data, gga);
+    while(1) {
+        GPS_UART_Fill_Buffer(buffer);
 
-    if (rmc_ptr != NULL) {
-        float time, latitude, longitude, knots;
-        char status, ns, ew;
+        const char *rmc = "$GPRMC"; // To get speed and so
+        const char *gga = "$GPGGA"; // To get satellite count
+        const char *rmc_ptr = NULL;
+        const char *gga_ptr = NULL;
+        float coordinates[COORDINATES_SIZE][2];
+        float (*coordinates_ptr)[2] = coordinates;
 
-        if (sscanf(rmc_ptr, "$GPRMC,%f,%c,%f,%c,%f,%c,%f,", &time, &status, &latitude, &ns, &longitude, &ew, &knots) == 7) {
-            (*coordinates_ptr)[0] = latitude;
-            (*coordinates_ptr)[1] = longitude;
+        rmc_ptr = strstr(buffer, rmc);
+        gga_ptr = strstr(buffer, gga);
+
+        if (rmc_ptr != NULL) {
+            float time, latitude, longitude, knots;
+            char status, ns, ew;
+
+            if (sscanf(rmc_ptr, "$GPRMC,%f,%c,%f,%c,%f,%c,%f,", &time, &status, &latitude, &ns, &longitude, &ew, &knots) == 7) {
+                (*coordinates_ptr)[0] = latitude;
+                (*coordinates_ptr)[1] = longitude;
+            }
+        }
+
+        if (gga_ptr != NULL) {
+            float time, latitude, longitude, knots, hdop, altitude;
+            char status, ns, ew;
+            int fix, satellites;
+
+            if (sscanf(gga_ptr, "$GPGGA,%f,%f,%c,%f,%c,%d,%d,%f,%f,",
+                       &time, &latitude, &ns, &longitude, &ew, &fix, &satellites, &hdop, &altitude) == 9) {
+                (*coordinates_ptr)[0] = latitude;
+                (*coordinates_ptr)[1] = longitude;
+            }
+        }
+
+        coordinates_ptr++;
+
+        if (coordinates_ptr - coordinates > COORDINATES_SIZE) {
+            coordinates_ptr = coordinates;
+            GPS_EEPROM_Save();
         }
     }
-
-    if (gga_ptr != NULL) {
-        float time, latitude, longitude, knots, hdop, altitude;
-        char status, ns, ew;
-        int fix, satellites;
-
-        if (sscanf(gga_ptr, "$GPGGA,%f,%f,%c,%f,%c,%d,%d,%f,%f,",
-                   &time, &latitude, &ns, &longitude, &ew, &fix, &satellites, &hdop, &altitude) == 9) {
-            (*coordinates_ptr)[0] = latitude;
-            (*coordinates_ptr)[1] = longitude;
-        }
-    }
-
-    coordinates_ptr++;
-
-    if (coordinates_ptr - coordinates > COORDINATES_SIZE)
-        coordinates_ptr = coordinates;
-
 }

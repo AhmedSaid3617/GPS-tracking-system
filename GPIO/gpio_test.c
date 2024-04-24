7976eab4a4c644c0b09db42ea2716fe547592003
OmarTamer2 <167299851+OmarTamer2@users.noreply.github.com>
2024-04-24 21:01:37 +0200

    Update gpio_test.c
    testing port f configuration

--- GPIO/gpio_test.c
+++ GPIO/gpio_test.c
@@ -5,4 +5,17 @@ int main(){
     GPIO_init((unsigned long)GPIO_PORTF_DATA_BITS_R, 0x11, 0xE);
     GPIO_write_pin((unsigned long)GPIO_PORTF_DATA_BITS_R, 1, 1);
     x =	(int)GPIO_read_pin((unsigned long)GPIO_PORTF_DATA_BITS_R, 1);
+    unsigned char button = 1;
+	GPIOF_default_init();
+	while(1){
+	if (read_sw1() & read_sw2()){
+		write_red_led(button);
+	}
+	else if(!read_sw1() & read_sw2()){
+		write_blue_led(button);
+	}
+	else if (read_sw1() & !read_sw2()){
+		write_green_led(button);
+	}
+}
 }

40de8995e267dc773fe2c72109971d67b707214b
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-04-26 21:42:51 +0300

    GPIO driver works on tiva c.

--- src/gpio.c
+++ src/gpio.c
@@ -107,11 +107,15 @@ void GPIO_write_pin(unsigned long GPIOPort, char pin_num, char value)
 
 void GPIOF_default_init(void)
 {
-    // configration of 3 leds
+    
+    GPIO_init(GPIOF, 0x11, 0xE);
+    
+    /* // configration of 3 leds
     SYSCTL_RCGCGPIO_R |= PF_mask; // configration of clock of portf
     while ((SYSCTL_PRGPIO_R & PF_mask) == 0)
         ;
     GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;
+    
     GPIO_PORTF_CR_R |= PF123_mask; // allow changes to pins 1,2,3
     GPIO_PORTF_AMSEL_R &= ~PF123_mask;
     GPIO_PORTF_AFSEL_R &= ~PF123_mask;
@@ -125,7 +129,8 @@ void GPIOF_default_init(void)
     GPIO_PORTF_PCTL_R &= ~0x000F000F;
     GPIO_PORTF_DIR_R &= ~PF04_mask; // enable input direction for pins 0,4
     GPIO_PORTF_DEN_R |= PF04_mask;
-    GPIO_PORTF_PUR_R |= PF04_mask;
+    GPIO_PORTF_PUR_R |= PF04_mask; */
+
 }
 unsigned char read_sw1(void)
 {

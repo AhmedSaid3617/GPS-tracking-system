dad689842a34bcb86ec122a9e0223e8d19c89b68
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-04-27 23:09:09 +0300

    Tested gps_uart_fill_buffer() and gps_uart_send_buffer().

--- src/main.c
+++ src/main.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
-int main() {
-    //GPS_Start();
+int main()
+{
+    // GPS_Start();
     uint8_t received;
     UART0_Init(9600);
     UART1_Init(9600);
-    char buffer[500];
+    char buffer[500] = {};
 
     while (1)
     {
-        if(gps_uart_fill_buffer(buffer, UART1) > 100){
+        if (gps_uart_fill_buffer(buffer, UART1) > 100)
+        {
             gps_uart_send_buffer(buffer, UART0);
         }
-        for (int i = 0; i < 10000000; i++);
     }
 }

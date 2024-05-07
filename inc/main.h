b2e97e6dc94eed0f55279c0e71af7c840cc27de0
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-05-07 12:56:18 +0300

    main.h

--- inc/main.h
+++ inc/main.h
@@ -1,11 +1,12 @@
 #ifndef GPS_TRACKING_SYSTEM_MAIN_H
 #define GPS_TRACKING_SYSTEM_MAIN_H
 
+
+#define COORDINATES_SIZE 250
+
 #include <stdint.h>
 #include "uart.h"
 #include "gps.h"
-
-#define COORDINATES_SIZE 250
 #include "Systic.h"
 
 #endif //GPS_TRACKING_SYSTEM_MAIN_H

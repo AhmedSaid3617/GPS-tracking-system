ec9a3ad141e313594e082874347598db6da9f3d2
Kareem <halabykareem@gmail.com>
2024-05-07 22:18:45 +0300

    Fixed Eeprom Read Array and added UART send float.

--- src/uart.c
+++ src/uart.c
@@ -11,17 +11,21 @@ void UART0_Init(int Baud_rate)
   SYSCTL_RCGCGPIO_R |= 0x01; // Set bit 0 for GPIO Port A
 
   // Wait for the clocks to be ready
-  while ((SYSCTL_PRGPIO_R & 0x01) == 0) {} // Check bit 0 for Port A
-  while ((SYSCTL_PRUART_R & (0x01)) == 0) {}  // Check bit 0 for UART0
+  while ((SYSCTL_PRGPIO_R & 0x01) == 0)
+  {
+  } // Check bit 0 for Port A
+  while ((SYSCTL_PRUART_R & (0x01)) == 0)
+  {
+  } // Check bit 0 for UART0
 
   // Configure PA0 (RX) and PA1 (TX) as UART pins
-  
+
   GPIO_PORTA_DEN_R |= (1 << 0) | (1 << 1);                               // Enable digital I/O for RX and TX pins
   GPIO_PORTA_AFSEL_R |= (1 << 0) | (1 << 1);                             // Set RX and TX to alternate function
   GPIO_PORTA_PCTL_R &= ~((GPIO_PORTA_PCTL_R & (0xF << 0)) | (0xF << 4)); // Clear PCTL bits for RX and TX
   GPIO_PORTA_PCTL_R |= (1 << 0) | (1 << 4);                              // Set PCTL bits for RX and TX
   GPIO_PORTA_AMSEL_R &= ~(0x03);                                         // Clear analog mode for RX and TX pins
-  
+
   // Set baud rate
   UART0_IBRD_R = (int)(SYSTEM_CLCK / (Baud_rate * 16));
   UART0_FBRD_R = ((SYSTEM_CLCK / (Baud_rate * 16)) - (int)(SYSTEM_CLCK / (Baud_rate * 16))) * 64;
@@ -33,10 +37,10 @@ void UART1_Init(int Baud_rate)
 {
 
   // Enable the clock for UART1
-  SYSCTL_RCGCUART_R |= (1<<1); // Set bit 1 for UART1
+  SYSCTL_RCGCUART_R |= (1 << 1); // Set bit 1 for UART1
 
   // Enable clock for GPIO Port B
-  SYSCTL_RCGCGPIO_R |= (1<<1); // Set bit 2 for GPIO Port B
+  SYSCTL_RCGCGPIO_R |= (1 << 1); // Set bit 2 for GPIO Port B
 
   // Wait for the clocks to be ready
   while ((SYSCTL_PRGPIO_R & (1 << 1)) == 0)
@@ -161,7 +165,6 @@ uint16_t UART_ReceiveByte(uint32_t UART_base, uint8_t *destination)
   return (uint16_t)0;
 }
 
-
 void UART_printf(char *buffer, uint32_t UART_base)
 {
   while (*buffer != 0)
@@ -171,4 +174,11 @@ void UART_printf(char *buffer, uint32_t UART_base)
   }
 }
 
-
+void UART0_print_float(float num)
+{
+  char output_buffer[15];
+  int first = (int)num;
+  int second = (num - first) * 10000;
+  sprintf(output_buffer, "%d.%d\n", first, second);
+  UART_printf(output_buffer, UART0);
+}

ec706832f617a23072e9ec43a42bca1fedace842
MohamedHussienMansour <mohamedhessien11202@gmail.com>
2024-05-08 23:36:29 +0300

    funtion systic handler to interupt when the user press in U in pc to receive data from Epprom

--- src/main.c
+++ src/main.c
@@ -6,34 +6,54 @@
 float coordinates[MAX_COORDINATES][2] = {};
 float (*coordinates_ptr)[2] = coordinates;
 float total_distance = 0;
+
 char buffer[GPS_BUFFER_SIZE];
-char oled_string[100];
-int mode = 0;
+int mode=0;
+char oledString[100];
+char led = 0;
+
 
 int main()
 {
     while (1)
     {
-        // gps fill buffer.
-        // gpio led.
-        // oled display.
+
+        // Uart_fill_buffer
+
+        // GPS_parse_and_push
+
+        // calculate_total_distance();
+
+        // display oled
+
+        //
+        GPIO_write_pin(GpioF, led, 1);
     }
-    
+
+
+
     return 1;
 }
+void SysTick_Handler(){
+    if (mode==0){
+        uint8_t data;
+        if (UART_ReceiveByte(UART_BASE, &data)) {  // read what in the UART and change the value in the data
+           if (data == 'U') {
+            unsigned long size=read_coordinates();  // take the size of data saved in Epprom to iterate over Epprom
 
-// Systick handler
-// if mode == 0
-    //
-    // if u:
-        // eeprom_read
-    // led = red
-    // oled_string = IDLE
-    // If gpio_sw1_read
-        // mode = 1
+            led = 2;
+            strcpy(oledString, "IDLE"); // changing the value Oled String
+            }}
+        if (read_sw1())
+            mode=1; // change mode to 1 if switch is on
 
 
+    }
 
+
+}
+
+//
 void calculate_total_distance(){
 
 }
@@ -45,3 +65,4 @@ void computer_upload_isr(){
 void eeprom_upload_isr(){
     // EEPROM_READ_ARRAY
 }
+

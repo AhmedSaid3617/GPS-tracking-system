6e286c337002be72966d8584a7ff0e8f303ebf3d
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-05-08 20:33:52 +0300

    Coordniates display proto-type, work in progress.

--- inc/main.h
+++ inc/main.h
@@ -1,12 +1,15 @@
 #ifndef GPS_TRACKING_SYSTEM_MAIN_H
 #define GPS_TRACKING_SYSTEM_MAIN_H
 #include <stdint.h>
+#include <stdio.h>
+#include <string.h>
 #include "i2c.h"
 #include "oled.h"
 #include "uart.h"
 #include "gps.h"
 #include "Systic.h"
 #include "eeprom.h"
+#include "utils.h"
 
 #define COORDINATES_SIZE 250
 
--- src/main.c
+++ src/main.c
@@ -1,12 +1,61 @@
-#include <stdio.h>
 #include "main.h"
-static char buff[200];
 
-int main() {
-    double decimal = 1.3445;
+gps_data_t data_point;
+char latitude_string[10] = "NaN";
+char longitude_string[10] = "NaN";
+char gps_status[10] = "Not valid";
+char satelites_string[10] = "0";
+unsigned int ticks = 0;
+
+int main()
+{
+    char gps_input_buffer[500] = {};
+    UART0_Init(9600);
+    UART1_Init(9600);
     I2C_Init();
     OLED_I2C_Init();
-    OLED_I2C_Write(0,0,"Test");
-    sprintf(buff, "Latitude: %d.%d", (int) decimal, (int)((decimal - (int) decimal) * 10000));
-    OLED_I2C_Write(0,0,buff);
+    // Systick_Interrupt_Init(100);
+    for (int i = 0; i < 10; i++)
+    {
+        OLED_I2C_Write(0, i, "                                      ");
+    }
+
+    OLED_I2C_Write(0, 0, gps_status);
+    OLED_I2C_Write(0, 2, longitude_string);
+    OLED_I2C_Write(0, 4, latitude_string);
+
+    while (1)
+    {
+        if (gps_uart_fill_buffer(gps_input_buffer, UART1) > 200)
+        {
+            //gps_uart_send_buffer(gps_input_buffer, UART0);
+            if (Gps_Parse(gps_input_buffer, &data_point))
+            {
+                strncpy(gps_status, "Valid    ", 10);
+                float_to_string(data_point.latitude, latitude_string);
+                float_to_string(data_point.longitude, longitude_string);
+                float_to_string(data_point.satellites, satelites_string);
+                OLED_I2C_Write(0, 0, gps_status);
+                OLED_I2C_Write(0, 2, longitude_string);
+                OLED_I2C_Write(0, 4, latitude_string);
+                OLED_I2C_Write(0, 6, satelites_string);
+                // UART_SendByte(UART0, 'a');
+            }
+        }
+    }
 }
+
+/* void SysTick_Handler()
+{
+    ticks++;
+
+    if (ticks % 10 == 0)
+    {
+        if (Gps_Parse(gps_input_buffer, &data_point))
+        {
+            strncpy(gps_status, "Valid    ", 10);
+            float_to_string(data_point.latitude, latitude_string);
+            float_to_string(data_point.longitude, longitude_string);
+        }
+    }
+} */

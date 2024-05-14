433fe17ef6bcf33761460570eeee1a9bcd108265
AhmedSaid3617 <said.ahmed3617@gmail.com>
2024-05-14 22:45:04 +0300

    Edited parameters.

--- src/main.c
+++ src/main.c
@@ -217,21 +217,3 @@ void update_display()
     OLED_I2C_Write(8 * 6, 7, buffer_status_string);
     OLED_I2C_Write(120, 7, heartbeat ? "." : " ");
 }
-
-float calculate_distance(const float x[2], const float y[2])
-{
-    double sum = 0;
-    sum += 2 * 6371 * 1000 * asin(sqrt(pow(sin((x[0] - y[0]) * (M_PI / 360)), 2) + cos(x[0] * (M_PI / 180)) * cos(y[0] * (M_PI / 180)) * pow(sin((y[1] - x[1]) * (M_PI / 360)), 2)));
-
-#ifdef DEBUG
-    UART_printf("\n++Change in distance\n", UART0);
-    UART0_print_float(x[0]);
-    UART0_print_float(x[1]);
-    UART0_print_float(y[0]);
-    UART0_print_float(y[1]);
-    UART0_print_float(sum);
-    UART_printf("\n++End Change in distance\n", UART0);
-#endif
-
-    return sum;
-}
